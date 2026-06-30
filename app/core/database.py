3ce6d646079653695ba5b0b3efca7ae90b2b6ab3
FEMI <techbyfemi@gmail.com>
2026-06-30 11:07:25 +0100

    chore(db): set up async SQLAlchemy engine and session

---
+++ app/core/database.py
@@ -0,0 +1,20 @@
+from app.core.config import settings
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessionmaker
+
+
+
+DATABASE_URL=settings.DATABASE_URL
+
+engine=create_async_engine(DATABASE_URL,
+                     pool_pre_ping=True,
+                     pool_recycle=300)
+sessionLocal=async_sessionmaker(engine, autoflush=False,expire_on_commit=False, class_=AsyncSession)
+class Base(DeclarativeBase):
+    pass
+async def get_db():
+    async with sessionLocal() as session:
+        yield session
+
+
+
